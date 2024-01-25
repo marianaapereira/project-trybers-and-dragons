ecc28210469fb3d4131e4b8d425a819d5071e35f
Mariana Pereira <marianapereira.s@live.com>
2024-01-25 16:45:31 -0300

    Requisito 1 - Criação da classe Race

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,1 +1,32 @@
-//
+// Esse número corresponde à quantidade de instâncias criadas a partir das classes estendidas da classe Race;
+// Cada raça terá seu número máximo de instâncias, que será definido dentro de cada classe especializada;
+// Na classe Race, o método deve lançar um erro com a mensagem Not implemented.
+
+abstract class Race {
+  private _name: string;
+  private _dexterity: number;
+
+  constructor(
+    name: string,
+    dexterity: number,
+  ) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+
+  abstract get maxLifePoints(): number;
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+}
+
+export default Race;
